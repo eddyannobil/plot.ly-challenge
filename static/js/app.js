509b8c0bd3860d7a5cc9d89b26ae30b23c569ca5
eddyannobil <edmundannobil@gmail.com>
2021-09-16 00:33:55 -0400

    Update files

---
+++ static/js/app.js
@@ -0,0 +1,15 @@
+// Function for change on dropdown menu
+function optionChanged(selectedID){
+
+    // Check if value is selected in dropdown
+    console.log(selectedID);
+ 
+    // Read the json file for the data
+    d3.json("data/samples.json").then((data) => {
+ 
+   //  console.log(data);
+ 
+
+    });
+}  
+
