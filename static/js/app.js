cf40522420e1bb4bd17ac4952a1b3f2e4ec1755f
eddyannobil <edmundannobil@gmail.com>
2021-09-16 17:12:43 -0400

    Update js file

--- static/js/app.js
+++ static/js/app.js
@@ -20,7 +20,25 @@ function optionChanged(selectedID){
         });
     // Selected value is passed
     d3.select("#selDataset").node().value = selectedID;
-
+    
+    // Filter Metadata for selected ID from dropdown
+    const idMetadata = data.metadata.filter(item=> (item.id == selectedID));
+       // {
+       //    console.log("------------------------")
+       //    console.log(item);
+       //    console.log(item.id);
+          
+       // });
+    // Check the metadata loaded for the selected ID
+    console.log(idMetadata);
+    
+    const panelDisplay = d3.select("#sample-metadata");
+    panelDisplay.html("");
+    Object.entries(idMetadata[0]).forEach(item=> 
+       {
+          // console.log(item);
+          panelDisplay.append("p").text(`${item[0]}: ${item[1]}`)
+       });
     });
 }  
 
