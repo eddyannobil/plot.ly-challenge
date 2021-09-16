9ae4e87c0d39720f57dba22cc59d922848f102d5
eddyannobil <edmundannobil@gmail.com>
2021-09-16 12:45:34 -0400

    Update js file

--- static/js/app.js
+++ static/js/app.js
@@ -7,8 +7,19 @@ function optionChanged(selectedID){
     // Read the json file for the data
     d3.json("data/samples.json").then((data) => {
  
-   //  console.log(data);
- 
+    // console.log(data);
+    
+    // Clears dropdown
+    d3.select("#selDataset").html("");   
+    
+    // Select the metadata array and for each item append the item ID and adds ID to dropdown
+    data.metadata.forEach(item =>
+        {
+         // console.log(item.id);
+        d3.select ("#selDataset").append('option').attr('value', item.id).text(item.id);
+        });
+    // Selected value is passed
+    d3.select("#selDataset").node().value = selectedID;
 
     });
 }  
